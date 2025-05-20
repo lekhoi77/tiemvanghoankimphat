9e72028afdf1296257cfc266872ad9e0d0e64794
Le Khoi da le factuer <khoivo.31221026006@st.ueh.edu.vn>
2025-05-20 11:08:28 +0700

    cmt

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,3 +1,10 @@
+// Cursor
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', e => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top  = e.clientY + 'px';
+});
+//Hero
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         var heroContent = document.querySelector('.hero-content');
